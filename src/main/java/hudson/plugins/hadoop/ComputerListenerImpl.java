642354f81f196ad1970f23ae9f63e149f7f1e375
kohsuke <kohsuke@dev.java.net>
2009-03-10 05:47:39 +0000

    starts TaskTracker

--- src/main/java/hudson/plugins/hadoop/ComputerListenerImpl.java
+++ src/main/java/hudson/plugins/hadoop/ComputerListenerImpl.java
@@ -8,6 +8,8 @@ import hudson.slaves.ComputerListener;
 import hudson.util.StreamTaskListener;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hdfs.server.datanode.DataNode;
+import org.apache.hadoop.mapred.TaskTracker;
+import org.apache.hadoop.mapred.JobConf;
 
 import java.io.File;
 import java.io.IOException;
@@ -25,6 +27,7 @@ public class ComputerListenerImpl extends ComputerListener {
             StreamTaskListener listener = new StreamTaskListener(System.out);
             Channel channel = PluginImpl.createHadoopVM(listener,c.getNode().createLauncher(listener));
             channel.call(new DataNodeStartTask(c.getNode().getRootPath().getRemote()));
+            channel.call(new TaskTrackerStartTask(c.getNode().getRootPath().getRemote()));
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -47,7 +50,7 @@ public class ComputerListenerImpl extends ComputerListener {
 
             Configuration conf = new Configuration();
             conf.set("fs.default.name","hdfs://localhost:12300/");
-            conf.set("dfs.data.dir",new File(new File(rootPath),"hadoop").getAbsolutePath());
+            conf.set("dfs.data.dir",new File(new File(rootPath),"hadoop/datanode").getAbsolutePath());
             conf.set("dfs.datanode.address", "127.0.0.1:0");
             conf.set("dfs.datanode.http.address", "127.0.0.1:0");
             conf.set("dfs.datanode.ipc.address", "127.0.0.1:0");
@@ -55,6 +58,33 @@ public class ComputerListenerImpl extends ComputerListener {
             DataNode dn = DataNode.instantiateDataNode(new String[0],conf);
             DataNode.runDatanodeDaemon(dn);
             
+            return null;
+        }
+
+        private static final long serialVersionUID = 1L;
+    }
+
+    /**
+     * Starts a {@link TaskTracker}.
+     */
+    private static class TaskTrackerStartTask implements Callable<Void,IOException> {
+        private final String rootPath;
+
+        private TaskTrackerStartTask(String rootPath) {
+            this.rootPath = rootPath;
+        }
+
+        public Void call() throws IOException {
+            System.out.println("Starting data node");
+
+            JobConf conf = new JobConf();
+            conf.set("slave.host.name", "localhost"); // TODO
+            conf.set("mapred.task.tracker.http.address","localhost:0");
+            conf.set("mapred.task.tracker.report.address","localhost:0");
+            conf.set("mapred.local.dir",new File(new File(rootPath),"hadoop/datanode").getAbsolutePath());
+
+            new Thread(new TaskTracker(conf)).start();
+
             return null;
         }
 
